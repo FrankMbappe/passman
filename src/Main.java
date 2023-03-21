9db7c75debea253475a1ec2ba9236016c08eb704
Frank Mbappe <kevyndrews@gmail.com>
2023-03-21 15:09:07 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Waiting for yo ass beach
+        System.out.println("fick ya beach");
+        System.out.println("fick ya");
+    }
+}
